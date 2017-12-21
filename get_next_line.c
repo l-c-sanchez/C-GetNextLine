4ff3568f64e4d4dc2880b39faff4f0ba2ea5ceed
Lea SANCHEZ <lesanche@student.42.fr>
2017-12-21 15:29:13 +0100

    push verifie !! mais erreurs de norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lesanche <lesanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/21 13:55:51 by lesanche          #+#    #+#             */
-/*   Updated: 2017/12/21 14:57:33 by lesanche         ###   ########.fr       */
+/*   Updated: 2017/12/21 15:20:51 by lesanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 void	ft_lstadd_end(t_list **begin_list, t_list *n)
 {
 	t_list *l;
-
 	l = *begin_list;
+
 	if (l == NULL)
 	{
 		*begin_list = n;
-		return ;
+		return;
 	}
 	while (l->next)
 		l = l->next;
 	l->next = n;
 }
 
-int		ft_shift_str_until(char *str, char c)
+
+int ft_shift_str_until(char *str, char c)
 {
 	int i;
 	int j;
@@ -39,23 +40,23 @@ int		ft_shift_str_until(char *str, char c)
 	if (i + 1 >= (int)ft_strlen(str))
 	{
 		str[0] = '\0';
-		return (1);
+		return (1);	
 	}
-	while (str[i + 1 + j] != '\0')
+	while(str[i+1+j] != '\0')
 	{
-		str[j] = str[i + 1 + j];
+		str[j] = str[i + 1 + j]; 
 		j++;
 	}
 	str[j] = '\0';
 	return (0);
 }
 
-int		line_len(t_list *begin_list)
+int get_line_len(t_list *begin_list)
 {
-	t_list	*elem;
-	int		len;
-	int		i;
-	char	*str;
+	t_list *elem;
+	int len;
+	int i;
+	char *str;
 
 	len = 0;
 	elem = begin_list;
@@ -71,7 +72,7 @@ int		line_len(t_list *begin_list)
 	return (len);
 }
 
-int		c_in_str(char *str, char c)
+int only_c_in_str(char *str, char c)
 {
 	int i;
 	int ct_c;
@@ -91,49 +92,57 @@ int		c_in_str(char *str, char c)
 	return (0);
 }
 
-void	del_content(void *content, size_t n)
+
+void del_content(void* content, size_t n)
 {
 	n++;
 	free(content);
 }
 
-int		get_next_line(const int fd, char **line)
+int get_next_line(const int fd, char **line)
 {
 	static char buf[BUFF_SIZE + 1];
-	int			nb;
-	t_list		*b_list;
-	t_list		*el;
+	int line_len;
+	int nb;
+	t_list *begin_list;
+	t_list *elem;
 
-	b_list = 0;
+	begin_list = 0;
+	if (fd < 0)
+		return(-1);
+	nb = 0;
 	while (1)
 	{
-		el = ft_lstnew(buf, BUFF_SIZE + 1);
-		ft_lstadd_end(&b_list, el);
+		elem = ft_lstnew(buf, BUFF_SIZE + 1);
+		ft_lstadd_end(&begin_list, elem);
 		if (ft_strchr(buf, '\n'))
-			break ;
-		if ((nb = read(fd, buf, BUFF_SIZE)) == 0)
-			break ;
-		if (nb == -1 || fd < 0)
-			return (-1);
+			break;
+		nb = read(fd, buf, BUFF_SIZE);
+		if (nb == -1)
+			return(-1);
 		buf[nb] = '\0';
+		if (nb == 0)
+			break;
 	}
-	if ((*line = (char*)malloc((line_len(b_list) + 1) * sizeof(char))) == NULL)
-		return (0);
+	if (nb == -1)
+		return(-1);
+	line_len = get_line_len(begin_list);
+	*line = (char*)malloc((line_len + 1) * sizeof(char));
 	*line[0] = '\0';
-	el = b_list;
-	while (el)
+	elem = begin_list;
+	while (elem)
 	{
-		ft_strncat(*line, el->content, line_len(b_list) - ft_strlen(*line));
-		el = el->next;
+		ft_strncat(*line, elem->content, line_len - ft_strlen(*line));
+		elem = elem->next;
 	}
-	ft_lstdel(&b_list, &del_content);
+	ft_lstdel(&begin_list, &del_content);
 	if ((int)ft_strlen(buf) == 1 && buf[0] == '\n')
 	{
-		buf[0] = '\0';
-		return (1);
+		buf[0]='\0';
+		return(1);
 	}
+	if ((nb < BUFF_SIZE) && (ft_strlen(*line) == 0) && only_c_in_str(buf, '\n') == 1)
+		return(0);
 	ft_shift_str_until(buf, '\n');
-	if ((nb < BUFF_SIZE) && (ft_strlen(*line) == 0) && c_in_str(buf, '\n') == 1)
-		return (0);
 	return (1);
 }
